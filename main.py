254568b353d251e6ca2723805d2fc12cecfe5e72
Mathys <75971034+opelooo@users.noreply.github.com>
2023-06-06 19:53:26 +0700

    made new patch endpoint

--- main.py
+++ main.py
@@ -33,6 +33,20 @@ async def predict_image(file: UploadFile = File(...)):
     # Return the predicted class
     return response
 
+@app.patch('/predict-patch')
+async def predict_image(file: UploadFile = File(...)):
+    # Read and preprocess the image
+    content = await file.read()
+    try:
+        image = Image.open(io.BytesIO(content))
+        image = func.preprocess_image(image)
+    except Exception as e:
+        return {"error" : str(e)}
+
+    response = func.predict(image)
+    # Return the predicted class
+    return response
+
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 8080))
 	run(app, host="0.0.0.0", port=port, timeout_keep_alive=1200)
